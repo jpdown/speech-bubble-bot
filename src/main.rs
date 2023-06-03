5b83292e061684103bcd1d89b45a75d98aea1603
Jaden Down <24631328+jpdown@users.noreply.github.com>
2023-06-03 11:04:26 -0500

    yoink helpful stuff from basic_structure example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use poise::serenity_prelude as serenity;
+use std::time::Duration;
 
+// Custom user data passed to all command functions
 struct Data {}
+
+// All command functions use these types
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
@@ -21,13 +42,27 @@ async fn main() {
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![age()],
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some(";".into()),
+                edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+                ..Default::default()
+            },
+            on_error: |error| Box::pin(on_error(error)),
             ..Default::default()
         })
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
-        .intents(serenity::GatewayIntents::non_privileged())
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
+                println!(
+                    "Logged in as {} in {} guilds",
+                    _ready.user.name,
+                    _ready.guilds.len()
+                );
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                // Initialize custom data here
                 Ok(Data {})
             })
         });
